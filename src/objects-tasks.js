07ac4fc35cd85b7d6c7b58201fc5894493deb68b
StanislavDevel <unfeel00@gmail.com>
2025-02-28 12:52:44 +0100

    feat: score 5/14/1

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,9 +17,7 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
-}
+const shallowCopy = (obj) => ({ ...obj });
 
 /**
  * Merges array of objects into a single object. If there are overlapping keys, the values
@@ -32,8 +30,20 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const sumObj = {};
+
+  objects.forEach((currentValue) => {
+    Object.keys(currentValue).forEach((key) => {
+      if (!(key in sumObj)) {
+        sumObj[key] = currentValue[key]; // Записываем первое значение
+      } else {
+        sumObj[key] += currentValue[key]; // Складываем, если число
+      }
+    });
+  });
+
+  return sumObj;
 }
 
 /**
@@ -49,8 +59,12 @@ function mergeObjects(/* objects */) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const { ...newObj } = obj;
+  keys.forEach((key) => {
+    delete newObj[key];
+  });
+  return newObj;
 }
 
 /**
