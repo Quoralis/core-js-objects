b02a6e8dead352e712c9cd4398d02ab82dd849c8
StanislavDevel <unfeel00@gmail.com>
2025-02-28 13:53:13 +0100

    feat: score 11/8/1

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -79,8 +79,8 @@ function removeProperties(obj, keys) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
 }
 
 /**
@@ -94,8 +94,8 @@ function compareObjects(/* obj1, obj2 */) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
 }
 
 /**
@@ -114,8 +114,8 @@ function isEmptyObject(/* obj */) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -128,8 +128,25 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+/**
+ * Returns a word from letters whose positions are provided as an object.
+ *
+ * @param {Object} lettersObject - An object where keys are letters and values are arrays of positions.
+ * @return {string} - The constructed word.
+ *
+ * @example
+ *   makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) // => 'aabbcc'
+ *   makeWord({ H: [0], e: [1], l: [2, 3, 8], o: [4, 6], W: [5], r: [7], d: [9] }) // => 'HelloWorld'
+ */
+function makeWord(lettersObject) {
+  const posLetterPairs = [];
+  Object.entries(lettersObject).forEach(([key, value]) => {
+    value.forEach((position) => {
+      posLetterPairs.push([position, key]);
+    });
+  });
+  posLetterPairs.sort((a, b) => a[0] - b[0]);
+  return posLetterPairs.map((pair) => pair[1]).join('');
 }
 
 /**
